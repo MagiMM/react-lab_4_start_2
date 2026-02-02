683e6eda90af27e5dddf95ee5efff3eb2c29dcb5
MarcinMag <marcin.magi@gmail.com>
2026-02-02 20:55:00 +0100

    Obsługa zdarzeń update

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let title = "Wall-E";
+    let title = 'Wall-E';
 
-  function ObslugaZdarzenia(Zdarzenie) {
-    console.log(Zdarzenie.target.value);
-    
-  }
-  return (
-    <div>
-      <h1>My favourite movies to watch</h1>
-      <h2>My favourite movie for today is {title}</h2>
-      <input type="text" onChange={ObslugaZdarzenia} />
-    </div>
-  );
+    function handleChange(event) {
+        console.log(event.target.value);
+    }
+
+    return (
+        <div>
+            <h1>My favourite movies to watch</h1>
+            <h2>My favourite movie for today is {title}</h2>
+            <input type="text" onChange={handleChange}/>
+        </div>
+    );
 }
 
 export default App;
