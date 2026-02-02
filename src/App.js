47a442c865936da3383f71b9126949ebac9491db
MarcinMag <marcin.magi@gmail.com>
2026-02-02 22:34:02 +0100

    Framework CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import "milligram";
 
 function App() {
     const [title, setTitle] = useState('Wall-E');
@@ -26,7 +27,7 @@ function App() {
 ];
 
     return (
-      <div>
+      <div className='container'>
           <h1>My favourite movies to watch</h1>
            <ul>{movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}</ul>
            <h2>My favourite movie for today is {title}</h2>
