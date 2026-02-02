69af4c7b133b34d5dc9373369d3d3651e7cb6c06
MarcinMag <marcin.magi@gmail.com>
2026-02-02 21:28:04 +0100

    Interakcje

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
             <h1>My favourite movies to watch</h1>
             <h2>My favourite movie for today is {title}</h2>
             {title.length > 0 && <div>{message}</div>}
-            <input type="text" value={title} onChange={handleChange}/>
+            <input type="text" value={title} onChange={handleChange}/> <button type='button' onClick={() => alert(title)}>Pokaż tytuł filmu</button> 
         </div>
     );
 }
